8569d7e719da03394822fd405bf899571e2f98b5
sadensmol <sadensmol@gmail.com>
2025-07-23 10:15:42 +0300

    wip

---
+++ docs/v1.0/core/sidebar.ts
@@ -0,0 +1,36 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  apisidebar: [
+    {
+      type: "doc",
+      id: "v1.0/core/swipe-games-public-core-api",
+    },
+    {
+      type: "category",
+      label: "UNTAGGED",
+      items: [
+        {
+          type: "doc",
+          id: "v1.0/core/create-new-game",
+          label: "Create new game",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "v1.0/core/create-new-free-rounds-campaign",
+          label: "Create new free rounds campaign",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "v1.0/core/cancel-delete-free-rounds-campaign",
+          label: "Cancel/Delete free rounds campaign",
+          className: "api-method delete",
+        },
+      ],
+    },
+  ],
+};
+
+export default sidebar.apisidebar;
--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
 # Swipe Games Public API
+
+## Requerments
+
+-   yarn
+
+## How to
+
+### Generate docs
+
+Run the following command to generate the API documentation:
+
+```bash
+make gen-docs
+```
