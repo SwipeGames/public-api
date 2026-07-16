021e2ea48f2916f91b6f2e1393b017f759eade53
sadensmol <sadensmol@gmail.com>
2026-07-16 13:43:40 +0400

    v1.8.1

--- api/v1.0/swipegames-integration/api.gen.zod.ts
+++ api/v1.0/swipegames-integration/api.gen.zod.ts
@@ -6,7 +6,7 @@
 It is used to make reverse calls to integrations working through Public API.
 Please implement this API on your side to support Swipe Games Public API.
 
- * OpenAPI spec version: 1.8.0
+ * OpenAPI spec version: 1.8.1
  */
 import * as zod from 'zod';
 
